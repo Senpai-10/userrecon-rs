5f105e4e05e4e22b50eabdbb592d593e64839ad0
senpai-10 <bmjfdrh@gmail.com>
2022-04-20 02:40:08 +0300

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,24 @@
-fn main() {}
+use reqwest;
+
+struct Urls {
+    name: String,
+    url: String,
+    found: bool,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut urls: Vec<Urls> = Vec::new();
+
+    for url in urls {
+        let res = reqwest::get(url.url.replace("{}", "")).await?;
+
+        if res.status() == 200 {
+            println!("Found!");
+        } else {
+            println!("Not found!");
+        }
+    }
+
+    Ok(())
+}
